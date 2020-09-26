0344ef0b2329e65d73b3e53bd58507a152cd608d
Jenny <jenny.kallerup@outlook.com>
2020-09-26 22:32:31 +0200

    Solve problem 1 day 1

--- 2015/day01/script.js
+++ 2015/day01/script.js
@@ -1,1 +1,15 @@
 import { day01 } from '../data.js';
+
+function problem01(){
+    let floor = 0;
+
+    day01.split('').forEach(c => {
+        floor += c == '(' ? 1 : -1;
+    });
+
+    return floor;
+}
+
+console.log(problem01());
+
+
