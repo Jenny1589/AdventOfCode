5544930596fa977e130eb65b1f2fea9a2fd0340c
Jenny <jenny.kallerup@outlook.com>
2020-09-27 00:40:29 +0200

    Solve problem 1 day 2

--- 2015/day02/script.js
+++ 2015/day02/script.js
@@ -1,1 +1,15 @@
 import { day02 } from '../data.js';
+
+let dimensions = day02.split(',')
+    .map(d => d.split('x'));
+    
+function problem01(){
+    return dimensions.map(d => {
+        let areas = [d[0]*d[1], d[1]*d[2], d[0]*d[2]]; 
+        let extra = Math.min(...areas);
+
+        return areas.reduce((acc, current) => acc + 2*current, 0) + extra;
+    }).reduce((acc, current) => acc + current);
+}
+
+console.log(problem01());
