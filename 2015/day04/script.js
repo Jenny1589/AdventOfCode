98eae47ba59e09bd0df959649b911a0b5afe07d2
Jenny <jenny.kallerup@outlook.com>
2020-09-27 17:41:46 +0200

    Solve problem 1 day 4

--- 2015/day04/script.js
+++ 2015/day04/script.js
@@ -1,3 +1,20 @@
 import { day04 } from '../data.js';
 import crypto from 'crypto-js';
 
+function problem01(){
+    return '00000'
+}
+
+function problemSolver(subStr){
+    let MD5 = 'ABCDE';
+    let number = -1;
+
+    while(MD5.substr(0, subStr().length) !== subStr()){
+        number++;
+        MD5 = crypto.MD5(day04 + number.toString()).toString();
+    }
+
+    return number;
+}
+
+console.log(problemSolver(problem01));
