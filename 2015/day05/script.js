7ed1359f6d473c23d4a979f00311f1ae174003c1
Jenny <jenny.kallerup@outlook.com>
2020-09-28 21:48:07 +0200

    Solve problem 2 day 5

--- 2015/day05/script.js
+++ 2015/day05/script.js
@@ -19,3 +19,44 @@ function problem01(){
 }
 
 console.log(problem01());
+
+function problem02(){
+    function hasRepeatedPair(word){
+        let pairs = [];
+        let pairFound = false;
+        let index = 0;
+
+        while(index < word.length - 1 && !pairFound){
+            let combo2Lt = word.substr(index, 2);
+            let i = index;
+
+            while(--i >= 0 && !pairFound){
+                pairFound = i !== pairs.length - 1 && pairs[i] === combo2Lt;
+            }
+
+            if(!pairFound) pairs.push(combo2Lt);
+
+            index++;
+        }
+
+        return pairFound;
+    }
+
+    function hasRepeatedLetter(word){
+        let letters = word.split('');
+        let repeatFound = false;
+        let index = -1;
+
+        while(++index < word.length - 2 && !repeatFound){       
+            repeatFound = letters[index] === letters[index + 2];
+        }
+
+        return repeatFound;
+    }
+
+    return day05.split(',')
+        .filter(s => hasRepeatedPair(s) && hasRepeatedLetter(s))
+        .length;
+}
+
+console.log(problem02());
