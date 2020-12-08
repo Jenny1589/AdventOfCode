97614651e43c22120c8deb9329a377332ffb6a2a
Jenny <jenny.kallerup@outlook.com>
2020-12-08 08:32:13 +0100

    Add puzzleData day08 2020

--- 2020/data.js
+++ 2020/data.js
@@ -11,5 +11,7 @@ export let
     day04 = fs.readFileSync('../files/day04_puzzleData.txt').toString(),
     day05 = fs.readFileSync('../files/day05_puzzleData.txt').toString().split('\r\n'),
     day06 = fs.readFileSync('../files/day06_puzzleData.txt').toString().split('\r\n'),
-    day07 = fs.readFileSync('../files/day07_puzzleData.txt').toString().split('\r\n');
+    day07 = fs.readFileSync('../files/day07_puzzleData.txt').toString().split('\r\n'),
+    day08 = fs.readFileSync('../files/day08_puzzleData.txt').toString().split('\r\n');
+
 
