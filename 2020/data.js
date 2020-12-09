777220c8fa1cc70524f93fff9d239b26ccde46dc
Jenny <jenny.kallerup@outlook.com>
2020-12-09 12:16:07 +0100

    Move split of data to data.js for day04 2020

--- 2020/data.js
+++ 2020/data.js
@@ -4,7 +4,7 @@ export let
     day01 = fs.readFileSync('../files/day01_puzzleData.txt').toString().split('\r\n'),
     day02 = fs.readFileSync('../files/day02_puzzleData.txt').toString().split('\r\n'),
     day03 = fs.readFileSync('../files/day03_puzzleData.txt').toString().split('\r\n'),
-    day04 = fs.readFileSync('../files/day04_puzzleData.txt').toString(),
+    day04 = fs.readFileSync('../files/day04_puzzleData.txt').toString().split('\r\n'),
     day05 = fs.readFileSync('../files/day05_puzzleData.txt').toString().split('\r\n'),
     day06 = fs.readFileSync('../files/day06_puzzleData.txt').toString().split('\r\n'),
     day07 = fs.readFileSync('../files/day07_puzzleData.txt').toString().split('\r\n'),
--- 2020/day04/script.js
+++ 2020/day04/script.js
@@ -1,28 +1,27 @@
 import {day04} from '../data.js'
 import Passport from './passport.js';
 
-const puzzleData = day04.split('\r\n')
-    .reduce((passportList, line) => {
-        const lineIsBlank = line === '';
+const puzzleData = day04.reduce((passportList, line) => {
+    const lineIsBlank = line === '';
 
-        if(passportList.length === 0 || lineIsBlank){
-            passportList.push(new Passport());
+    if(passportList.length === 0 || lineIsBlank){
+        passportList.push(new Passport());
+    }
+
+    if(!lineIsBlank){
+        const info = line.split(/[:\s]/g);
+        const passportIndex = passportList.length - 1;
+
+        for(let i = 0; i < info.length; i += 2){
+            const passport = passportList[passportIndex];
+            const fieldName = info[i];
+            
+            const fieldVal = info[i + 1];
+            passport.addInformation(fieldName, fieldVal);
         }
-
-        if(!lineIsBlank){
-            const info = line.split(/[:\s]/g);
-            const passportIndex = passportList.length - 1;
-
-            for(let i = 0; i < info.length; i += 2){
-                const passport = passportList[passportIndex];
-                const fieldName = info[i];
-                
-                const fieldVal = info[i + 1];
-                passport.addInformation(fieldName, fieldVal);
-            }
-        }
-        return passportList;
-    }, []);
+    }
+    return passportList;
+}, []);
 
 const solution1 = () => puzzleData.filter(passport => passport.hasAllRequiredFields()).length;
 const solution2 = () => puzzleData.filter(passport => passport.hasAllRequiredFields() && passport.valuesAreValid()).length;
