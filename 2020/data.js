1d6dbbd528b3eec0bf3a1ac05edc45593e0e181a
Jenny <jenny.kallerup@outlook.com>
2020-12-09 12:21:14 +0100

    Create helper method convertFileToArrayOfStrings
    All files seems to be read and split the same way.  To adapt the principles of DRY I created a help method to convert file content into an array of strings.

--- 2020/data.js
+++ 2020/data.js
@@ -1,14 +1,16 @@
 import fs from 'fs';
 
+const convertFileToArrayOfStrings = (filePath) => fs.readFileSync(filePath).toString().split('\r\n');
+
 export let
-    day01 = fs.readFileSync('../files/day01_puzzleData.txt').toString().split('\r\n'),
-    day02 = fs.readFileSync('../files/day02_puzzleData.txt').toString().split('\r\n'),
-    day03 = fs.readFileSync('../files/day03_puzzleData.txt').toString().split('\r\n'),
-    day04 = fs.readFileSync('../files/day04_puzzleData.txt').toString().split('\r\n'),
-    day05 = fs.readFileSync('../files/day05_puzzleData.txt').toString().split('\r\n'),
-    day06 = fs.readFileSync('../files/day06_puzzleData.txt').toString().split('\r\n'),
-    day07 = fs.readFileSync('../files/day07_puzzleData.txt').toString().split('\r\n'),
-    day08 = fs.readFileSync('../files/day08_puzzleData.txt').toString().split('\r\n'),
-    day09 = fs.readFileSync('../files/day09_puzzleData.txt').toString().split('\r\n');
+    day01 = convertFileToArrayOfStrings('../files/day01_puzzleData.txt'),
+    day02 = convertFileToArrayOfStrings('../files/day02_puzzleData.txt'),
+    day03 = convertFileToArrayOfStrings('../files/day03_puzzleData.txt'),
+    day04 = convertFileToArrayOfStrings('../files/day04_puzzleData.txt'),
+    day05 = convertFileToArrayOfStrings('../files/day05_puzzleData.txt'),
+    day06 = convertFileToArrayOfStrings('../files/day06_puzzleData.txt'),
+    day07 = convertFileToArrayOfStrings('../files/day07_puzzleData.txt'),
+    day08 = convertFileToArrayOfStrings('../files/day08_puzzleData.txt'),
+    day09 = convertFileToArrayOfStrings('../files/day09_puzzleData.txt');
 
 
